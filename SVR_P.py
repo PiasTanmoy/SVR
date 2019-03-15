1d1e64038c5b00dc5c61817a9bec4b730b62d2c6
Pias <pias1600@gmail.com>
2019-03-15 11:26:00 +0600

    Initial commit

---
+++ SVR_P.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 14 02:28:25 2019
+
+@author: Pias Tanmoy
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Position_Salaries.csv')
